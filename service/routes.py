06788e1d9b12b17e50fa7ca7dfa831284e89c903
Adam soro <adamsoro0321@gmail.com>
2024-12-27 01:23:20 -0500

    first commit

--- service/routes.py
+++ service/routes.py
@@ -109,6 +109,19 @@ def create_products():
 #
 # PLACE YOUR CODE HERE TO READ A PRODUCT
 #
+@app.route("/product/<int:product_id>",methods = ['GET'])
+def get_products(product_id):
+    """retreive the product where id 
+    
+    this endpoint will return a product
+    """
+    app.logger.info("Reqquest to Retreive a product with [%s]",product_id)
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    pp.logger.info("Returning product: %s", product.name)
+    return product.serialize(), status.HTTP_200_OK    
 
 ######################################################################
 # U P D A T E   A   P R O D U C T
